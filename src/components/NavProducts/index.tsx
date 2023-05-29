bfdf3c12648c1f14134a25db8fcfa0fec3e1a73f
5pamm3r <cmassarino.dev@gmail.com>
2023-05-29 19:18:36 -0300

    add navProducts component

--- src/components/NavProducts/index.tsx
+++ src/components/NavProducts/index.tsx
@@ -1,46 +1,42 @@
-import React from "react";
+import React, {useContext} from "react";
 
 import arrowLeft from "../../assets/icons/arrow-left.svg";
 import arrowRight from "../../assets/icons/arrow-right.svg";
-import {Product} from "~/types/typeProduct";
+import {SearchContext} from "~/Context/searchContext";
 
 import style from "./NavProducts.module.scss";
 
-interface Props {
-  currentPage: number;
-  setCurrentPage: (page: number) => void;
-  productsPerPage: number;
-  products: Product[];
-}
-const NavProducts: React.FC<Props> = ({
-  children,
-  currentPage,
-  setCurrentPage,
-  productsPerPage,
-  products,
-}) => {
-  const onNextPage = () => {
-    if (productsPerPage * currentPage < products.length) {
-      setCurrentPage(currentPage + 1);
-    }
-  };
-  const onPrevPage = () => {
-    if (productsPerPage * currentPage > 16) {
-      setCurrentPage(currentPage - 1);
-    }
-  };
+const NavProducts: React.FC = ({children}) => {
+  const {
+    state: {startIndex, totalProducts, endIndex, itemsPerPage},
+    actions: {setStartIndex},
+  } = useContext(SearchContext);
 
   return (
     <div className={style.navContainer}>
-      <span className={style.countProducts}>{productsPerPage * currentPage} of 32 products</span>
       {children}
-      <div className={style.buttonContainer}>
-        <button onClick={onPrevPage}>
-          <img alt="arrow left" src={arrowLeft} />
-        </button>
-        <button onClick={onNextPage}>
-          <img alt="arrow right" src={arrowRight} />
-        </button>
+      <div className={style.navProducts}>
+        <div className={style.navProductsContainer}>
+          <span className={style.countProducts}>
+            {endIndex} of {totalProducts} products
+          </span>
+          <div className={style.buttonContainer}>
+            <button
+              disabled={startIndex === 0}
+              onClick={() => setStartIndex(Math.max(startIndex - itemsPerPage, 0))}
+            >
+              <img alt="arrow left" src={arrowLeft} />
+            </button>
+            <button
+              disabled={endIndex >= totalProducts}
+              onClick={() =>
+                setStartIndex(Math.min(startIndex + itemsPerPage, totalProducts - itemsPerPage))
+              }
+            >
+              <img alt="arrow right" src={arrowRight} />
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );

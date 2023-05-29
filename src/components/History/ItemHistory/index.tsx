5527e29553e24a34c33aff054cbfd724ad8e0492
5pamm3r <cmassarino.dev@gmail.com>
2023-05-29 19:18:18 -0300

    add itemHistory component

--- src/components/History/ItemHistory/index.tsx
+++ src/components/History/ItemHistory/index.tsx
@@ -2,20 +2,23 @@ import React from "react";
 
 import {History} from "~/types/typeHistory";
 import coin from "~/assets/icons/coin.svg";
+import {Product} from "~/types/typeProduct";
 
 import styles from "./ItemHistory.module.scss";
 interface Props {
   name: History["name"];
   image: History["img"];
   price: History["cost"];
+  category: History["category"];
 }
-const ItemHistory: React.FC<Props> = ({name, image, price}) => {
+const ItemHistory: React.FC<Props> = ({name, image, price, category}) => {
   return (
     <div className={styles.itemContainer}>
       <div className={styles.imgContainer}>
         <img alt={name} src={image.url} />
       </div>
-      <div className={styles.nameContainer}>
+      <div className={styles.infoContainer}>
+        <span className={styles.category}>{category}</span>
         <span>{name}</span>
       </div>
       <div className={styles.priceContainer}>

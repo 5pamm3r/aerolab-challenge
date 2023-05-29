8e0aa8b97a21604941704cb548fc8dc673f32442
5pamm3r <cmassarino.dev@gmail.com>
2023-05-29 19:19:52 -0300

    add search

--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -1,22 +1,30 @@
-import React from "react";
+import React, {useContext} from "react";
 
 import {Product} from "~/types/typeProduct";
+import {SearchContext} from "~/Context/searchContext";
 
 import style from "./Product.module.scss";
 
 interface Props {
   products: Product[];
   render: (value: Product) => React.ReactNode;
-  currentPage: number;
-  productsPerPage: number;
 }
 
-const Products: React.FC<Props> = ({products, render, currentPage, productsPerPage}) => {
-  const startIndex = (currentPage - 1) * productsPerPage;
-  const endIndex = startIndex + productsPerPage;
+const Products: React.FC<Props> = ({products, render}) => {
+  const {
+    state: {searchValue, startIndex, endIndex},
+  } = useContext(SearchContext);
 
   return (
-    <ul className={style.listContainer}>{products.slice(startIndex, endIndex).map(render)}</ul>
+    <ul className={style.listContainer}>
+      {searchValue
+        ? products
+            .filter((e) => {
+              return e.name.toLowerCase().includes(searchValue.toLowerCase());
+            })
+            .map(render)
+        : products.slice(startIndex, endIndex).map(render)}
+    </ul>
   );
 };
 

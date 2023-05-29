82f75d526d8576e7da25c8c8e6a75788726b8752
5pamm3r <cmassarino.dev@gmail.com>
2023-05-29 19:16:37 -0300

    add categories component

---
+++ src/components/Categories/index.tsx
@@ -0,0 +1,30 @@
+import React, {useContext} from "react";
+
+import UserContext from "../../Context/userContext";
+import {Product} from "~/types/typeProduct";
+
+import styles from "./Categories.module.scss";
+
+interface Props {
+  render: (value: string) => React.ReactNode;
+}
+const Categories: React.FC<Props> = ({render}) => {
+  const {
+    state: {products},
+  } = useContext(UserContext);
+  const uniqueCategories: Set<string> = new Set();
+
+  products.forEach((product: Product) => {
+    if (product.category !== "PC Accesories") {
+      uniqueCategories.add(product.category);
+    }
+  });
+
+  return (
+    <div className={styles.categories}>
+      <ul className={styles.categoriesContainer}>{Array.from(uniqueCategories).map(render)}</ul>
+    </div>
+  );
+};
+
+export default Categories;

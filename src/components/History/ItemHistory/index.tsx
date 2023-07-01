719455e52b6486965a4ef972e5fc048a0c61838f
5pamm3r <cmassarino.dev@gmail.com>
2023-06-30 21:27:29 -0300

    Add cost and count

--- src/components/History/ItemHistory/index.tsx
+++ src/components/History/ItemHistory/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 
 import {History} from "~/types/typeHistory";
 import coin from "~/assets/icons/coin.svg";
-import {Product} from "~/types/typeProduct";
 
 import styles from "./ItemHistory.module.scss";
 interface Props {
@@ -10,22 +9,33 @@ interface Props {
   image: History["img"];
   price: History["cost"];
   category: History["category"];
+  count: History["count"];
+  cost: History["cost"];
 }
-const ItemHistory: React.FC<Props> = ({name, image, price, category}) => {
+const ItemHistory: React.FC<Props> = ({name, image, price, category, count, cost}) => {
   return (
-    <div className={styles.itemContainer}>
-      <div className={styles.imgContainer}>
-        <img alt={name} src={image.url} />
-      </div>
-      <div className={styles.infoContainer}>
-        <span className={styles.category}>{category}</span>
-        <span>{name}</span>
-      </div>
-      <div className={styles.priceContainer}>
-        <span className={styles.price}>{price}</span>
-        <img alt="coin" src={coin} />
-      </div>
-    </div>
+    <>
+      <li className={styles.itemContainer}>
+        <div className={styles.imgContainer}>
+          <img alt={name} src={image.url} />
+        </div>
+        <div className={styles.infoContainer}>
+          <span className={styles.category}>{category}</span>
+          <span>{name}</span>
+          <span>${cost}</span>
+        </div>
+        <div className={styles.countContainer}>
+          <span>x{count}</span>
+        </div>
+        <div className={styles.priceContainer}>
+          <span className={styles.price}>{price * count}</span>
+          <div className={styles.coinImgContainer}>
+            <img alt="coin" src={coin} />
+          </div>
+        </div>
+      </li>
+      <hr />
+    </>
   );
 };
 

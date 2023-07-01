3779cd226fee29edd6b4a25c22375abcdf255c92
5pamm3r <cmassarino.dev@gmail.com>
2023-06-30 21:27:04 -0300

    Add cost and count

--- src/components/History/index.tsx
+++ src/components/History/index.tsx
@@ -12,17 +12,19 @@ const History = () => {
   } = useContext(UserContext);
 
   return (
-    <div className={styles.history}>
+    <ul className={styles.history}>
       {history.map((product: History) => (
         <ItemHistory
           key={product.createDate}
           category={product.category}
+          cost={product.cost}
+          count={product.count}
           image={product.img}
           name={product.name}
           price={product.cost}
         />
       ))}
-    </div>
+    </ul>
   );
 };
 
